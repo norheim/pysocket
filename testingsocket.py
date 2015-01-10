3d23d5e0dd3706842473e4c22aeb1ca6afabee2e
Johannes Norheim <norheim@mit.edu>
2015-01-10 11:47:53 -0500

    changed tests to account for new tellClients function

--- testingsocket.py
+++ testingsocket.py
@@ -3,10 +3,7 @@ import time
 
 serverKeeper = ServerKeeper(2005)
 
-def responseFunc():
-    for i in range(1, 10000):
-        serverKeeper.tellClients(str(i/20.0)+','+str(i/20.0))
-        print i
-        time.sleep(0.1)
-
-serverKeeper.waitForConnection(responseFunc)
+for i in range(1, 10000):
+    serverKeeper.tellClients(str(i)+','+str(i))
+    print i
+    time.sleep(0.1)
